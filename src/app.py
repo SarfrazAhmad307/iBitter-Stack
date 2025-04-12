aec1321661c11ef40dd06e761b036b1446179208
Sarfraz Ahmad <sarfaraz_076@outlook.com>
2025-04-12 15:51:41 +0400

    Fixed image path

--- src/app.py
+++ src/app.py
@@ -67,7 +67,7 @@ elif selected == "Algorithm":
     with col2:
         # add image
         st.image(
-            "static/pipeline.svg",
+            "src/static/pipeline.svg",
             # caption="Algorithm Overview",
             use_container_width=True,
         )
